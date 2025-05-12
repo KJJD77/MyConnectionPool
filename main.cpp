3cda6d015339e0cfe38362f474ec81dc193ec27f
kcz <1350063100@qq.com>
2025-05-12 15:34:37 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include"Connection.h"
+#include<iostream>
+
+int main()
+{
+/*     Connection conn;
+    if (!conn.connect("localhost", 3306, "root","*****", "test"))
+    {
+        std::cerr << "连接数据库失败\n";
+        return 1;
+    }
+    char sql[1024]={0};
+    sprintf(sql,"INSERT INTO user (name,age,sex) VALUES ('%s', %d, '%s')", "zhangsan", 20, "男");
+    conn.update(sql); */
+
+    return 0;
+}
---
+++ public.h
@@ -0,0 +1,3 @@
+#pragma once
+#include <iostream>
+#define LOG(str) std::cerr << __FILE__ << ' ' << __LINE__ << ' ' << __TIMESTAMP__ << ' ' << str << std::endl;
